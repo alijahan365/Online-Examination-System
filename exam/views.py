cd033149b4486b3c64f7dfae4dc3db039587ecd3
alijahan365 <jahan242-15-846@diu.edu.bd>
2026-08-27 21:00:12 +0600

    Fix 404 afterlogin redirects for student and teacher dashboards

--- exam/views.py
+++ exam/views.py
@@ -31,16 +31,17 @@ def is_student(user):
 
 def afterlogin_view(request):
     if is_student(request.user):      
-        return redirect('student/student-dashboard')
+        return redirect('student-dashboard')
                 
     elif is_teacher(request.user):
         accountapproval=TMODEL.Teacher.objects.all().filter(user_id=request.user.id,status=True)
         if accountapproval:
-            return redirect('teacher/teacher-dashboard')
+            return redirect('teacher-dashboard')
         else:
             return render(request,'teacher/teacher_wait_for_approval.html')
     else:
         return redirect('admin-dashboard')
+
 
 
 
